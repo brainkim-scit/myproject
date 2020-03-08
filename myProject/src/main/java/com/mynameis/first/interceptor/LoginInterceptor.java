ff8f7fce0d099c13c6b58617fb21b351f7dd8f95
scitmaster <scitmaster@scitmaster-PC>
2020-03-08 17:33:56 +0900

    인터셉터 추가

--- myProject/src/main/java/com/mynameis/first/interceptor/LoginInterceptor.java
+++ myProject/src/main/java/com/mynameis/first/interceptor/LoginInterceptor.java
@@ -7,9 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class LoginIntereptor extends HandlerInterceptorAdapter {
+public class LoginInterceptor extends HandlerInterceptorAdapter {
 	
-	private static final Logger logger = LoggerFactory.getLogger(LoginIntereptor.class);
+	private static final Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
